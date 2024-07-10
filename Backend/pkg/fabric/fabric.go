181157f8a07fe61d17be7797dc6f3d9965e75367
Sourabh-Trellis <dsourabh@trellissoft.ai>
2024-07-10 19:12:16 +0530

    initial commit with GoTransact project in Gin framework and Hyperledger Fabric

---
+++ Backend/pkg/fabric/fabric.go
@@ -0,0 +1,121 @@
+package fabric
+
+import (
+	"GoTransact/config"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	fconfig "github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+)
+
+var Smartcontract *gateway.Contract
+
+func InitFabric() {
+	log.Println("============ application-golang starts ============")
+
+	err := os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
+	if err != nil {
+		log.Fatalf("Error setting DISCOVERY_AS_LOCALHOST environment variable: %v", err)
+	}
+
+	walletPath := "wallet"
+	// remove any existing wallet from prior runs
+	os.RemoveAll(walletPath)
+	wallet, err := gateway.NewFileSystemWallet(walletPath)
+	if err != nil {
+		log.Fatalf("Failed to create wallet: %v", err)
+	}
+
+	if !wallet.Exists("appUser") {
+		err = populateWallet(wallet)
+		if err != nil {
+			log.Fatalf("Failed to populate wallet contents: %v", err)
+		}
+	}
+
+	ccpPath := filepath.Join(
+		// "..",
+		// "..",
+		// "test-network",
+		config.Fabric_path,
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"connection-org1.yaml",
+	)
+
+	gw, err := gateway.Connect(
+		gateway.WithConfig(fconfig.FromFile(filepath.Clean(ccpPath))),
+		gateway.WithIdentity(wallet, "appUser"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to connect to gateway: %v", err)
+	}
+	defer gw.Close()
+
+	channelName := "mychannel"
+	if cname := os.Getenv("CHANNEL_NAME"); cname != "" {
+		channelName = cname
+	}
+
+	log.Println("--> Connecting to channel", channelName)
+	network, err := gw.GetNetwork(channelName)
+	if err != nil {
+		log.Fatalf("Failed to get network: %v", err)
+	}
+
+	chaincodeName := "goTransact"
+	if ccname := os.Getenv("CHAINCODE_NAME"); ccname != "" {
+		chaincodeName = ccname
+	}
+
+	log.Println("--> Using chaincode", chaincodeName)
+	contract := network.GetContract(chaincodeName)
+	Smartcontract = contract
+	fmt.Println(Smartcontract)
+}
+
+func populateWallet(wallet *gateway.Wallet) error {
+	log.Println("============ Populating wallet ============")
+	credPath := filepath.Join(
+		// "..",
+		// "..",
+		// "test-network",
+		config.Fabric_path,
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"users",
+		"User1@org1.example.com",
+		"msp",
+	)
+
+	certPath := filepath.Join(credPath, "signcerts", "cert.pem")
+	// read the certificate pem
+	cert, err := os.ReadFile(filepath.Clean(certPath))
+	if err != nil {
+		return err
+	}
+
+	keyDir := filepath.Join(credPath, "keystore")
+	// there's a single file in this dir containing the private key
+	files, err := os.ReadDir(keyDir)
+	if err != nil {
+		return err
+	}
+	if len(files) != 1 {
+		return fmt.Errorf("keystore folder should have contain one file")
+	}
+	keyPath := filepath.Join(keyDir, files[0].Name())
+	key, err := os.ReadFile(filepath.Clean(keyPath))
+	if err != nil {
+		return err
+	}
+
+	identity := gateway.NewX509Identity("Org1MSP", string(cert), string(key))
+
+	return wallet.Put("appUser", identity)
+}
